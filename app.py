2ce172a6dc43eff7180f952dbb8aab021c8767b4
Тринеев Дмитрий Сергеевич <D:\dimas\save\THEBAT>
2018-01-02 23:59:07 +0300

    убран порт из-за ошибки R10

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ from routes import setup_routes
 app = web.Application()
 setup_routes(app)
 #log.debug('Start server')
-web.run_app(app, host='127.0.0.1', port=8080)
+#web.run_app(app, host='127.0.0.1', port=8080)
+web.run_app(app)
 
